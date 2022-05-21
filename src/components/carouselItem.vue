ac075b26c79c3092ce4387730eaf4aeb0bfef994
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-05-21 13:08:04 +0200

    creato e predisposto component -carouselItem-

---
+++ src/components/carouselItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="carouselItem" v-show="visibleItem === index">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default  {
+    props: [
+        'visibleItem',
+        'index'
+    ],
+    data: function() {
+        return {
+
+        }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
