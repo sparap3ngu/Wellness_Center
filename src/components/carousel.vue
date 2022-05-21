11a2f1d1ed1cc28bfb1299f4a2476623007c422d
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-05-21 13:07:35 +0200

    creato e predisposto component -carousel-

---
+++ src/components/carousel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="carousel">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default  {
+    data: function() {
+        return {
+
+        }
+    }
+
+  }
+</script>
+
+
+
+<style scoped>
+
+</style>
