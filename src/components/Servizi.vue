08f84fc6fd427e47a44478fd7009bcc3aa5964c1
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-05-26 21:29:00 +0200

    creato 2 component in più per far tratt e servizi

--- src/components/Servizi.vue
+++ src/components/Servizi.vue
@@ -1,64 +1,16 @@
 <template>
-  <div class="page-container">
-    <!--<md-card v-for="s in Servizi" :key="s.id">
-      <md-card-media>
-        <img :src="s.immagine" alt="People">
-      </md-card-media>-->
+<div>
+  <md-tabs class="md-transparent" md-alignment="centred">
+      <md-tab id="tab-home" md-label="Tratt" to="/tratt"></md-tab>
+      <md-tab id="tab-pages" md-label="Mass" to="/mass"></md-tab>
+    </md-tabs>
+    
 
-    <md-card>
-      <md-card-media>
-        <img src="/vuematerial/assets/examples/card-image-1.jpg" alt="People">
-      </md-card-media>
-
-      <md-card-header>
-        <div class="md-title">Titolo </div>
-        <div class="md-subhead"> Sottotitolo</div>
-      </md-card-header>
-
-      <md-card-expand>
-        <md-card-actions md-alignment="right">
-
-            <md-button to="/dettaglio">Dettagli</md-button>
-  
-        </md-card-actions>
-
-        <md-card-expand-content>
-          <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-          </md-card-content>
-        </md-card-expand-content>
-      </md-card-expand>
-    </md-card>
-  </div>
+</div>
 </template>
 
 <script>
-export default  {
-
-    data: function () {
-      Servizi: []
-    },
-
-created () {
-  db.collection ('Servizi')
-    .get()
-    .then(function(res) {
-        res.forEach(function(doc) {
-            console.log(doc.id, " => ",  doc.data());
-            const data = {
-              'id': doc.id,
-              'nome': doc.data().nome,
-              'costo':doc.data().costo,
-              'immagine': doc.data().immagine,
-            }
-            this.Servizi.push(data)
-
-        });
-    });
-}
-  }
 </script>
 
-
-<style scoped>
+<style>
 </style>
