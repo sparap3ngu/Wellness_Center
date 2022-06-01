f613b9d7bf421fb0bf663833c255246040e4888b
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-06-01 11:58:12 +0200

    provato con metodo del prof, ma non funzia

--- src/components/Dettaglio.vue
+++ src/components/Dettaglio.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="page-container">
-    <p>
-      Dettaglio
-    </p>
+    <span>{{s.nome}}</span>
+    <span>{{s.costo}}</span>
+    <img :src="s.immagine" alt="immagine del servizio">
   </div>
 </template>
 
 <script>
+import db from '../main'
+
 export default  {
+  data: function (){
+    return {
+      s: null,
+    }
+  },
+
+  created: function () {
+    db.collection ('Servizi')
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach (doc => {
+        const data =  {
+          'id': doc.id,
+          'immagine': doc.data().immagine,
+          'nome': doc.data().nome,
+          'costo': doc.data().costo,
+        }
+        this.s.push(data)
+        this.$route.id.then(data => {
+          this.s = data.data;
+        })
+      })
+    }) 
+  }
 
   }
 </script>
