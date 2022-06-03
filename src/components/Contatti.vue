6b949f764826d717f13407406d0de185d2401be3
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-06-03 14:44:31 +0200

    comentato e sistemato il codice del  component contatti

--- src/components/Contatti.vue
+++ src/components/Contatti.vue
@@ -1,72 +1,62 @@
 <template>
   <div class="Contatti"> 
 
+<!--Sezione contatti-->
     <h1>
       I nostri contatti
     </h1>
 
-
-      <div class="md-layout"  >
+      <div class="md-layout">
+<!--Primo elemento (telefono)-->
         <div class="md-layout-item  md-xlarge-size-50 md-xsmall-size-100">
           <md-list class="md-double-line">
-        <md-subheader>Telefono</md-subheader>
- 
-        <md-list-item>
-        <md-icon class="md-primary">phone</md-icon>
- 
-        <div class="md-list-item-text">
-        <span>Reception +39 0461 281111</span>
-        </div>
-       
-        </md-list-item>
-        </md-list>
+            <md-subheader>Telefono</md-subheader>
+            <md-list-item>
+            <md-icon class="md-primary">phone</md-icon>
+            <div class="md-list-item-text">
+              <span> +39 0461 281111</span>
+            </div>
+            </md-list-item>
+          </md-list>
         </div>
      
-        
+<!--Secondo elemento (email)-->
         <div class="md-layout-item  md-xlarge-size-50 md-xsmall-size-100">
           <md-list class="md-double-line">
-        <md-subheader>E-mail</md-subheader>
- 
-        <md-list-item>
-        <md-icon class="md-primary">email</md-icon>
- 
-        <div class="md-list-item-text">
-        <span>info@wellnesscenter.it</span>
+          <md-subheader>E-mail</md-subheader>
+            <md-list-item>
+            <md-icon class="md-primary">email</md-icon>
+            <div class="md-list-item-text">
+              <span>info@wellnesscenter.it</span>
+            </div>
+            </md-list-item>
+          </md-list>
         </div>
-        </md-list-item>
-     
-        </md-list>
-        </div>
-        
       </div>
       <md-divider></md-divider>
 
+<!--Sezione del personale-->
     <h1>
       Conosci il nostro team!
     </h1>
 
+<!--Impostazione del layout delle cards del personale-->
       <div class="md-layout" >
-      <div class="md-layout-item md-xlarge-size-10 md-large-size-20 md-medium-size-25 md-xsmall-size-50" 
-      v-for="s in Personale" :key="s.id">
-        <div >
-      <md-card id="cards" class="md-elevation-0">
-        <md-card-media class="immagini">
-          <img id="immagini" :src="s.immagine" alt="People" >
-        </md-card-media>
-
-        <md-card-header>
-          <div class="md-title">{{s.nome}} <br> {{s.cognome}}</div>
-          <div class="md-subhead"> {{s.ruolo}}</div>
-        </md-card-header>
-      </md-card>
+        <div class="md-layout-item md-xlarge-size-10 md-large-size-20 md-medium-size-25 md-xsmall-size-50" 
+          v-for="s in Personale" :key="s.id">
+          <div >
+            <md-card id="cards" class="md-elevation-0">
+              <md-card-media class="immagini">
+                <img id="immagini" :src="s.immagine" alt="People" >
+              </md-card-media>
+              <md-card-header>
+                <div class="md-title">{{s.nome}} <br> {{s.cognome}}</div>
+                <div class="md-subhead"> {{s.ruolo}}</div>
+              </md-card-header>
+          </md-card>
         </div>
-      
       </div>
     </div>
-
-
-
-
   </div>     
 </template>
  
@@ -78,11 +68,13 @@ export default  {
   name:"Contatti",
   data: function () {
       return{
+//Array che contiene tutto il personale
          Personale: []
       }
     },
 
 created () {
+//Funzione che passa tutti i dati dei dipendenti all'array Personale
   db.collection ('Personale')
     .get()
     .then(querySnapshot => {
@@ -99,27 +91,16 @@ created () {
  
   }
 }
- 
 </script>
+
 <style scoped>
-#immagine{
-  width:100px;
-  height:100px;
-  display: inline;
-  position: absolute;
-
-}
-
+/* CSS per la pagina */
 h1 {
   text-align: center;
   margin-top: 50px;
 }
 
-#div {
-  position: relative;
-  align-content: center;
-}
-
+/* CSS per le cards */
 #cards{
   text-align:center;
   padding: 20px;
