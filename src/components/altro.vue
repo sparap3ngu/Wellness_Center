c17673c6c50f3abd2b5acee7489e2800971a2761
Giulia Mussetti <giuliamussetti01@gmail.com>
2022-06-03 12:53:37 +0200

    sistemato e commentato il codice in massaggi e altro

--- src/components/altro.vue
+++ src/components/altro.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="page-container">
+
+<!--Tabs di navigazione tra le due sezioni di trattamenti -->
      <md-tabs class="md-primary" md-alignment="fixed" md-active-tab="tab-altro">
             <md-tab id="tab-massaggi" md-label="Massaggi" to="/trattamenti/massaggi"> </md-tab>
             <md-tab id="tab-altro" md-label="Altro" to="/trattamenti/altro" exact> </md-tab>
           </md-tabs>
 
+<!--Contenuto della pagina-->
       <h1> Altri trattamenti </h1>
 
+<!--Impostazione del layout di cards-->
     <div class="md-layout" >
       <div class="md-layout-item md-large-size-20 md-xlarge-size-20 md-medium-size-33 md-xsmall-size-100" 
       v-for="t in Trattamenti" :key="t.id">
-        <div >
+
+<!--Impostazione della card-->
+    <div >
       <md-card id="cards">
         <md-card-media class="immagini">
           <img id="immagini" :src="t.immagine" alt="People" >
@@ -21,6 +27,7 @@
           <div class="md-subhead"> {{t.costo}}</div>
         </md-card-header>
 
+<!--Navigazione al dettaglio-->
           <md-card-actions md-alignment="right">
               <md-button :to="{name: 'dettaglioAltro', params: {id: t.id}}">Dettagli</md-button>
           </md-card-actions>
@@ -39,11 +46,13 @@ import db from '../main'
 export default  {
     data: function () {
       return{
+//Array che raccoglie tutti i trattamenti del db
          Trattamenti: []
       }
     },
 
   created () {
+//Funzione che, nonappena viene creata la pagina, prende i dati dal db e li passa all'array Trattamenti
   db.collection ('Trattamenti')
     .get()
     .then(querySnapshot => {
@@ -64,30 +73,24 @@ export default  {
 
 
 <style scoped>
+/* CSS per la pagina */
 h1{
     text-align: center;
     margin-top: 50px;
-
 }
+
 #cards{
   margin-top:40px;
 
 }
- .md-card-header {
-  height:100px;
-}  
 
+/* CSS per le cards */
 .immagini  {
   height: 200px;
   overflow: hidden;
 }
-
 .md-title {
   overflow: hidden;
   text-overflow:ellipsis;  
 }
-
-
-
-
 </style>
--- src/components/massaggi.vue
+++ src/components/massaggi.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="page-container">
+
+<!--Tabs di navigazione tra le due sezioni di trattamenti -->
      <md-tabs class="md-primary" md-alignment="fixed" md-active-tab="tab-massaggi" >
             <md-tab id="tab-massaggi" md-label="Massaggi" to="/trattamenti/massaggi"> </md-tab>
             <md-tab id="tab-altro" md-label="Altro" to="/trattamenti/altro"> </md-tab>
           </md-tabs>
 
+<!--Contenuto della pagina-->
     <h1> Massaggi </h1>
 
+<!--Impostazione del layout di cards-->
     <div class="md-layout" >
       <div class="md-layout-item md-large-size-20 md-xlarge-size-20 md-medium-size-33 md-xsmall-size-100" 
       v-for="m in Massaggi" :key="m.id">
-        <div >
+
+<!--Impostazione della card-->
+    <div >
       <md-card id="cards">
         <md-card-media class="immagini">
           <img id="immagini" :src="m.immagine" alt="People" >
@@ -21,6 +27,7 @@
           <div class="md-subhead"> {{m.costo}}</div>
         </md-card-header>
 
+<!--Navigazione al dettaglio-->
           <md-card-actions md-alignment="right">
               <md-button :to="{name: 'dettaglioMassagi', params: {id: m.id}}">Dettagli</md-button>
           </md-card-actions>
@@ -39,11 +46,13 @@ import db from '../main'
 export default  {
     data: function () {
       return{
+//Array che raccoglie tutti i massaggi del db
          Massaggi: []
       }
     },
 
   created () {
+//Funzione che, nonappena viene creata la pagina, prende i dati dal db e li passa all'array Trattamenti
   db.collection ('Massaggi')
     .get()
     .then(querySnapshot => {
@@ -64,6 +73,7 @@ export default  {
 
 
 <style scoped>
+/* CSS per la pagina */
 h1 {
   text-align: center;
   margin-top: 50px;
@@ -72,10 +82,8 @@ h1 {
 #cards{
   margin-top:40px;
 }
- .md-card-header {
-  height:100px;
-}  
 
+/* CSS per le cards */
 .immagini  {
   height: 200px;
   overflow: hidden;
@@ -85,8 +93,4 @@ h1 {
   overflow: hidden;
   text-overflow:ellipsis;  
 }
-
-
-
-
 </style>
